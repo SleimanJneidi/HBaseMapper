c6c6b31eaa4201a92e409112b5ed5ae44038e24a
Sleiman Jneidi <jneidi.sleiman@gmail.com>
2014-12-10 01:04:12 +0000

    adds testing for HTableHandler

---
+++ src/test/java/com/rolonews/hbasemapper/TestHTableHandler.java
@@ -0,0 +1,46 @@
+package com.rolonews.hbasemapper;
+
+import com.rolonews.hbasemapper.com.rolonews.hbasemapper.hbasehandler.HTableHandler;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestHTableHandler {
+
+    @Mock
+    private HTableInterface table;
+
+    @Mock
+    private HConnection connection;
+
+    @Captor
+    private ArgumentCaptor putCaptor;
+
+    @Mock
+    private Configuration configuration;
+
+
+    @Test
+    public void testInsertRecord() throws Exception {
+
+        TableName tableName = TableName.valueOf("Person");
+        when(connection.isTableAvailable(tableName)).thenReturn(true);
+        when(connection.getTable(tableName)).thenReturn(table);
+        when(connection.getConfiguration()).thenReturn(configuration);
+
+        HTableHandler tableHandler = new HTableHandler(connection);
+        HTableInterface  hTableInterface =  tableHandler.getOrCreateHTable(Person.class);
+        assertNotNull(hTableInterface);
+
+    }
+}
